ac29d9e42c7d29457410b062fa67a1d1c1b278f4
Utkarsh 976 <utkarshtripathiofficialll@gmail.com>
2026-03-23 13:46:21 +0530

    initial commit

---
+++ Namaste_Duniyaa.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Namaste Duniyaa"<<endl;    
+    cout<<"Namaste Duniyaa"<<'\n';    
+    cout<<"Namaste Duniyaa\n";
+    cout<<"Namaste Duniyaa";
+    cout<<"\n";
+
+    int a = 'a';
+    cout<<a<<endl;
+
+    char ch = 98;
+    cout<<ch<<endl;
+
+    // char ch1 = 123456;
+    // cout<<ch1<<endl;
+
+    unsigned int e = 1;
+    cout<<e<<endl;
+
+    int f = 3;
+    cout<<!f<<'\n';
+
+    int d = 0;
+    cout<<!d;
+
+    return 0;
+}
