48a1a0f85648afe8fab17c2e396a1f8ef2a7e28a
Utkarsh 976 <utkarshtripathiofficialll@gmail.com>
2026-03-24 19:28:04 +0530

    I Revised About Conditionals And Loops

---
+++ Conditional_And_Loops.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // int a ,b ;
+
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+
+    // if(a>b){
+    //     cout<<"a is greater "<<endl;
+    // }
+    //     // else{
+    //     //     cout<<"b is greater";
+    //     // }
+    // if(b>a){
+    //     cout<<"b is greater ";
+    // }
+
+    int a;
+    a = cin.get();
+    cout<<a;
+
+    return 0;
+}
