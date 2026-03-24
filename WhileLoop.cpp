0e4c12f08e844904916920158167cc626e3c195e
Utkarsh 976 <utkarshtripathiofficialll@gmail.com>
2026-03-24 19:34:11 +0530

    Revised About while Loops

---
+++ WhileLoop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int i = 1;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+
+    while(i<=a){
+        cout<<i<<" ";
+        i = i+1;
+    }
+
+
+    return 0;
+}
