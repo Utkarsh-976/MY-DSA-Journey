5c5590cd8a8590a64a9e8d345e060655c6f73477
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-09-09 23:19:37 +0530

    Add files via upload

---
+++ Recursion/print_n_to_1_recur.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void print_n_to_1(int i, int n){
+    if (i<1)
+    return;
+
+    cout<<i<<endl;
+    print_n_to_1(i-1,n);
+    
+}
+
+int main(){
+    int n;
+    cout<<"type:";
+
+    cin>>n;
+    print_n_to_1(n,n);
+}
