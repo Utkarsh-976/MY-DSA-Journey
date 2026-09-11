3c61ecaf00fa30d06ccbf70d2c52e37a26e82bed
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-09-11 23:18:28 +0530

    Add files via upload

---
+++ Recursion/factorial_parameterized.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void factorial(int i, int fact)
+{
+    if (i < 1)
+    {
+        cout << fact;
+        return;
+    }
+
+    factorial(i - 1, fact * i);
+}
+
+int main()
+{
+    int n;
+    cout << "type:";
+    cin >> n;
+
+    factorial(n,1);
+    
+    return 0;
+}
