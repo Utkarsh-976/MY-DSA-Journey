e8b1f0e37d90767211f8461cab49fbba4b51492c
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-09-12 23:19:02 +0530

    Add files via upload

---
+++ Recursion/reverse_an_array_2_variable.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void rev(int arr[], int l, int r)
+{
+    if (l >= r)
+    {
+        return;
+    }
+
+    swap(arr[l], arr[r]);
+    rev(arr, l + 1, r - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "type: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "type arr: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    rev(arr, 0, n - 1);
+
+    cout << "rev arr: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
